746de6d90b77c53856b9bbe1f6dd83a13bc0f96a
Michael Lawrence BSc (HONS) <m2lawrence77@outlook.com>
2016-12-18 16:03:33 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/* C# Mikes user input through single dimensional array. 
+ */
+using System;
+
+namespace InputArrays2016
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Mike.\n\n");
+			
+			int[] numbers = new int[5];
+			
+			for (int i= 0; i<5; i++) 
+			{
+				//ask for user input:
+				Console.WriteLine("Enter 5 numbers:");
+				//store in array:
+				numbers[i]= int.Parse(Console.ReadLine());
+			}
+			
+			//loop through array.
+			foreach (int j in numbers) {
+				//write array.
+				Console.WriteLine(j);
+			}
+			
+			//pause to view and exit:
+			Console.Write("Exit Mike...");
+			Console.ReadKey(true);
+		}
+	}
+}
+
